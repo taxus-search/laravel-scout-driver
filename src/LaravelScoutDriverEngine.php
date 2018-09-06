233c88d1049edeb303f10cfaf5639188c795d2c0
taxus-ir <34268741+taxus-ir@users.noreply.github.com>
2018-09-07 01:34:21 +0430

    Update LaravelScoutDriverEngine.php
    Final tests finished and it is ready to use

--- src/LaravelScoutDriverEngine.php
+++ src/LaravelScoutDriverEngine.php
@@ -11,38 +11,52 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class LaravelScoutDriverEngine extends Engine
 {
-  /**
-   * Index where the models will be saved.
-   *
-   * @var string
-   */
   protected $index;
 
-  /**
-   * 
-   *
-   * @var object
-   */
   protected $taxus;
 
-  /**
-   * Create a new engine instance.
-   *
-   * @param 
-   * @return void
-   */
   public function __construct()
   {
 
 
   }
 
+  private function getTaxusUniqueId($model)
+  {
+    $searchableModel = $model->toSearchableArray();
+    $id=$model[$model->getKeyName()];
+    if($searchableModel['type']!=null)
+    {
+      return $searchableModel['type'].'-'.$id;
+    }
+    else
+    {
+      return $id;
+    }
+  }
+
+  private function getModelId($item,$model)
+  {
+    $id=$item->id;
+    if($item->type!=null)
+    {
+      $id=preg_replace('/'.$item->type.'-'.'/', '', $id, 1);
+      return $id;
+    }
+    else
+    {
+      return $id;
+    }
+  }
+
   /**
    * Update the given model in the index.
    *
-   * @param  Collection  $models
+   * @param  \Illuminate\Database\Eloquent\Collection  $models
+   * @throws \AlgoliaSearch\AlgoliaException
    * @return void
    */
+
   public function update($models)
   {
     if ($models->isEmpty()) {
@@ -50,21 +64,23 @@ class LaravelScoutDriverEngine extends Engine
     }
     foreach ($models as $model)
     {
-      $model = $model->toSearchableArray();
-      $model['type'] = $model['type'].'-'.$model['id'];
-      $model = json_encode($model);
-    ob_start();
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL,config('taxus.base_url').'/items');
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-      'Authorization: '.config('taxus.authorization'),
-      'Content-Type: application/json'
-    ));
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $model);
+      $id=$this->getTaxusUniqueId($model);
 
-    $server_output = curl_exec ($ch);
-    curl_close ($ch);
+      $model = $model->toSearchableArray();
+      $model['id'] = $id;
+      $model = json_encode($model);
+      ob_start();
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL,config('taxus.base_url').'/items');
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Authorization: '.config('taxus.authorization'),
+        'Content-Type: application/json'
+      ));
+      curl_setopt($ch, CURLOPT_POST, 1);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $model);
+
+      $server_output = curl_exec ($ch);
+      curl_close ($ch);
     }
 
 
@@ -73,21 +89,21 @@ class LaravelScoutDriverEngine extends Engine
   /**
    * Remove the given model from the index.
    *
-   * @param  Collection  $models
+   * @param  \Illuminate\Database\Eloquent\Collection  $models
    * @return void
    */
+
   public function delete($models)
   {
     if ($models->isEmpty()) {
       return;
     }
     foreach ($models as $model) {
-      $model = $model->toSearchableArray();
-
+      $id=$this->getTaxusUniqueId($model);
 
       ob_start();
       $ch = curl_init();
-      curl_setopt($ch, CURLOPT_URL, config('taxus.base_url').'/items/' . $model->id);
+      curl_setopt($ch, CURLOPT_URL, config('taxus.base_url').'/items/' . $id);
       curl_setopt($ch, CURLOPT_HTTPHEADER, array(
         'Authorization: ' . config('taxus.authorization'),
         'Content-Type: application/json'
@@ -101,50 +117,91 @@ class LaravelScoutDriverEngine extends Engine
   }
 
   /**
-   * Perform the given search on the engine.
+   * Run the given search on the engine.
    *
-   * @param  Builder  $builder
+   * @param  \Laravel\Scout\Builder  $builder
    * @return mixed
    */
+
   public function search(Builder $builder)
   {
-
+    return $this->executeQuery($builder);
   }
 
   /**
-   * Perform the given search on the engine.
+   * Paginate the given search on the engine.
    *
-   * @param  Builder  $builder
+   * @param  \Laravel\Scout\Builder  $builder
    * @param  int  $perPage
    * @param  int  $page
    * @return mixed
    */
+
   public function paginate(Builder $builder, $perPage, $page)
   {
+    $from = ($page - 1) * $perPage;
 
+    return $this->executeQuery($builder, $from, $perPage);
   }
 
-  /**
-   * Perform the given search on the engine.
-   *
-   * @param  Builder  $builder
-   * @param  array  $options
-   * @return mixed
-   */
-  protected function performSearch(Builder $builder, array $options = [])
-  {
+  private function executeQuery(&$builder, $from = 0, $limit = null){
+
+
+    $filters = "";
+    foreach ($this->filters($builder) as $filter)
+    {
+      $filters .= $filter . '&';
+    }
+
+
+    $filters = rtrim($filters, '&');
+
+    $curl_url = config('taxus.base_url').'/search/' . config('taxus.search_api_key').'/?query='.urlencode($builder->query);
+    $defaultFieldList='fields=id,type';
+    $curl_url .='&'.$defaultFieldList;
+
+    $curl_url .='&'.'from='.$from;
+
+    if($limit!=null)
+    {
+      $curl_url .='&'.'limit='.$limit;
+    }
+
+
+    if(!empty($filters))
+    {
+      $curl_url .='&'.$filters;
+    }
+
+
+    ob_start();
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $curl_url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+      'Content-Type: application/json'
+    ));
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $server_output = json_decode( curl_exec($ch) );
+    curl_close($ch);
+
+    return $server_output;
 
   }
 
   /**
    * Get the filter array for the query.
    *
-   * @param  Builder  $builder
+   * @param  \Laravel\Scout\Builder  $builder
    * @return array
    */
+
   protected function filters(Builder $builder)
   {
-
+    return collect($builder->wheres)->map(function ($value, $key) {
+      return 'filter['.$key.']='.urlencode($value);
+    })->values()->all();
   }
 
   /**
@@ -153,8 +210,13 @@ class LaravelScoutDriverEngine extends Engine
    * @param  mixed  $results
    * @return \Illuminate\Support\Collection
    */
+
   public function mapIds($results)
   {
+    $ids = [];
+    foreach($results->result->items as $document)
+      $ids[] = $document->id;
+    return collect($ids);
 
   }
 
@@ -165,8 +227,20 @@ class LaravelScoutDriverEngine extends Engine
    * @param  \Illuminate\Database\Eloquent\Model  $model
    * @return Collection
    */
+
   public function map($results, $model)
   {
+    if ( count($results->result->items) === 0) {
+      return Collection::make();
+    }
+
+    return Collection::make($results->result->items)->map(function ($item) use ($model)
+    {
+      $obj = new $model;
+      $id =$this->getModelId($item,$model);
+      $obj = $model->find($id);
+      return $obj;
+    })->filter()->values();
 
   }
 
@@ -176,19 +250,10 @@ class LaravelScoutDriverEngine extends Engine
    * @param  mixed  $results
    * @return int
    */
+
   public function getTotalCount($results)
   {
-    return 0;
+    return count($results->result->items);
   }
 
-  /**
-   * Generates the sort if theres any.
-   *
-   * @param  Builder $builder
-   * @return array|null
-   */
-  protected function sort($builder)
-  {
-
-  }
 }
