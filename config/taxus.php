66b57badf44a44b4353fea841aebb1d694d0420f
taxus-ir <34268741+taxus-ir@users.noreply.github.com>
2018-09-16 17:08:43 +0430

    Update taxus.php

--- config/taxus.php
+++ config/taxus.php
@@ -1,8 +1,7 @@
 <?php
 
 return [
-  'authorization' => env('TAXUS_SECRET', [YOUR_SECRET]),
-  'base_url' => env('TAXUS_BASE_URL', 'https://api.taxus.ir/v1')
-  'search_api_key' => env('TAXUS_SEARCH_API_KEY', [SEARCH_API_KEY])
-
+  'authorization' => env('TAXUS_SECRET', 'YOUR_SECRET'),
+  'base_url' => env('TAXUS_BASE_URL', 'https://api.taxus.ir/v1'),
+  'search_api_key' => env('TAXUS_SEARCH_API_KEY', 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
 ];
